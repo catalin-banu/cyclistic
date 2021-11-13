2b551498a5aabd02493f82f26a194eeea684a4ed
Catalin <cata_banu98@yahoo.com>
2021-11-13 21:07:02 +0200

    Add navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import WelcomeComponent from "./components/welcome-component/welcome-component";
+import {BrowserRouter as Router, Routes , Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<WelcomeComponent/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/granular-components/navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Nav, Navbar, Button} from "react-bootstrap";
+
+
+export default function NavbarComponent(){
+    return(
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="/">Nume aplicație</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="#biciclete">Biciclete</Nav.Link>
+                    <Nav.Link href="#trotinete">Trotinete electrice</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+                <Button className="me-2" variant="outline-warning">Închiriază acum</Button>
+                <Button variant="outline-success">Admin</Button>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
