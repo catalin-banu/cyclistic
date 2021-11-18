0a1b6d317b0b701f9e5fd3edcaebac58fe7379ef
Catalin <cata_banu98@yahoo.com>
2021-11-18 19:58:18 +0200

    Create mapping for orders list

--- client/src/components/admin/view-orders-component.jsx
+++ client/src/components/admin/view-orders-component.jsx
@@ -1,52 +1,81 @@
 import React from "react";
 import Table from 'react-bootstrap/Table';
 
+const orders = [
+    {
+        lastName:"Popescu",
+        firstName: "Ion",
+        orderNO: "0001",
+        email:"popescuion@mail.com",
+        phone:"0756378928",
+        product: "Bicicleta de oras",
+        rentalTime:{
+            hours: 1,
+            days: 0,
+        },
+        status: "in asteptare"
+    },
+    {
+        lastName:"Ionescu",
+        firstName: "Vasile",
+        orderNO: "0002",
+        email:"vasi_pup@gmail.com",
+        phone:"0754578928",
+        product: "Bicicleta de munte",
+        rentalTime:{
+            hours: 0,
+            days: 1,
+        },
+        status: "in asteptare"
+    },
+    {
+        lastName:"Georgescu",
+        firstName: "Cristi",
+        orderNO: "0003",
+        email:"cristi_g@yahoo.com",
+        phone:"0754578928",
+        product: "Bicicleta electrica",
+        rentalTime:{
+            hours: 6,
+            days: 0,
+        },
+        status: "in asteptare"
+    }
+]
+
 function ViewOrdersComponent(){
+
+    let ordersList = orders.map(
+        (order) =>
+        <tr>
+            <td>{orders.indexOf(order) + 1}</td>
+            <td>{order.orderNO}</td>
+            <td>{order.lastName}</td>
+            <td>{order.firstName}</td>
+            <td>{order.email}</td>
+            <td>{order.phone}</td>
+            <td>{order.product}</td>
+            <td>{order.rentalTime.hours + " h " + order.rentalTime.days +" zile"}</td>
+            <td>{order.status}</td>
+        </tr>
+    )
     return(
         <Table striped bordered hover responsive>
             <thead>
                 <tr>
-                <th>#</th>
-                <th>Nr. Comenzii</th>
-                <th>Nume</th>
-                <th>Prenume</th>
-                <th>Email</th>
-                <th>Telefon</th>
-                <th>Produs</th>
-                <th>Interval inchiriere</th>
+                    <th>#</th>
+                    <th>Nr. Comenzii</th>
+                    <th>Nume</th>
+                    <th>Prenume</th>
+                    <th>Email</th>
+                    <th>Telefon</th>
+                    <th>Produs</th>
+                    <th>Interval inchiriere</th>
+                    <th>Status</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@twitter</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>@twitter</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Thornton</td>
-                    <td>@twitter</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>Thornton</td>
-                    <td>@twitter</td>
-                </tr>
+               {ordersList}
             </tbody>
         </Table>
     )
