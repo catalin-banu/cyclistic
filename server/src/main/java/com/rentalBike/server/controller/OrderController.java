8a512a69b6880a7f511f9443d908be25fcdd9509
Catalin <cata_banu98@yahoo.com>
2021-11-19 21:25:51 +0200

    Update controller

--- server/src/main/java/com/rentalBike/server/controller/OrderController.java
+++ server/src/main/java/com/rentalBike/server/controller/OrderController.java
@@ -3,11 +3,14 @@ package com.rentalBike.server.controller;
 import com.rentalBike.server.model.OrderItem;
 import com.rentalBike.server.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/v1/api")
 public class OrderController {
 
     @Autowired
@@ -15,17 +18,27 @@ public class OrderController {
 
     @GetMapping("/orders")
     private List<OrderItem> getAllOrders(){
-        return orderService.findAll();
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/order/{id}")
+    private ResponseEntity<OrderItem> getOrderById(@PathVariable Long id){
+        return new ResponseEntity<OrderItem>(orderService.getOrderById(id), HttpStatus.OK);
     }
 
     @PostMapping("/order")
-    private OrderItem addOrder(@RequestBody OrderItem orderItem){
-        orderService.save(orderItem);
-        return orderItem;
+    private ResponseEntity<OrderItem> addOrder(@RequestBody OrderItem orderItem){
+        return new ResponseEntity<OrderItem>(orderService.saveOrder(orderItem), HttpStatus.CREATED);
     }
 
     @PutMapping("/order/{id}")
-    private OrderItem updateOrder(@RequestBody OrderItem orderItem, @PathVariable Long id){
-        return orderService.update(orderItem,id);
+    private ResponseEntity<OrderItem> updateOrder(@RequestBody OrderItem orderItem, @PathVariable Long id){
+        return new ResponseEntity<OrderItem>(orderService.updateOrder(orderItem,id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/order/{id}")
+    private ResponseEntity<String> deleteOrder(@PathVariable Long id){
+        orderService.deleteOrder(id);
+        return new ResponseEntity<String>("Order deleted successfully!.", HttpStatus.NO_CONTENT);
     }
 }
