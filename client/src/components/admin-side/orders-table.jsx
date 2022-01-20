4f15605a2d468430f4dd53dc7ec0ef047d5cea71
Catalin <cata_banu98@yahoo.com>
2022-01-20 19:14:21 +0200

    Add statistics

--- client/src/components/admin-side/orders-table.jsx
+++ client/src/components/admin-side/orders-table.jsx
@@ -1,64 +1,93 @@
 import React, { useEffect, useState } from "react";
-import {Table, Alert} from 'react-bootstrap';
+import { Table, Alert } from "react-bootstrap";
 import axios from "axios";
 import ViewOrderDetails from "./view-order-details";
 
-function OrdersTable(){
-    const[ordersToShow, setOrdersToShow] = useState([]);
-    const[errorMessage, setErrorMessage] = useState(false);
-    const[orderView, setOrderView] = useState('');
-    const[orderModalShow, setOrderModalShow] = useState(false);
+function OrdersTable() {
+  const [ordersToShow, setOrdersToShow] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(false);
+  const [orderView, setOrderView] = useState("");
+  const [orderModalShow, setOrderModalShow] = useState(false);
+  const [numberOfOrders, setNumberOfOrders] = useState(0);
 
-    let ordersList = ordersToShow.map((order) =>
-        <tr key={order.id} onClick={() =>{ setOrderView(order); setOrderModalShow(true)}}>
-            <td>{order.id}</td>
-            <td>{order.lastName}</td>
-            <td>{order.firstName}</td>
-            <td>{order.phone}</td>
-            <td><p>{order.productList}</p></td>
-            <td>{order.rentalTime}</td>
-            <td>{order.status}</td> 
-            <td>{order.price} Ron</td> 
-        </tr>
-    )
+  const getTotalComands = () => {
+    setNumberOfOrders(ordersToShow.length);
+  };
 
-    useEffect(() => {
-        const recevingData = async() => {
-            try{
-                const response = await axios.get("http://localhost:8080/v1/api/orders");
-                const responseData = await response.data;
-                setOrdersToShow(responseData);
-            }catch(error){
-                setErrorMessage(true);
-                console.log(error.response);
-            }
-        }
-        recevingData();
-    },[ordersToShow]);
+  let ordersList = ordersToShow.map((order) => (
+    <tr
+      key={order.id}
+      onClick={() => {
+        setOrderView(order);
+        setOrderModalShow(true);
+      }}
+    >
+      <td>{order.id}</td>
+      <td>{order.lastName}</td>
+      <td>{order.firstName}</td>
+      <td>{order.phone}</td>
+      <td>
+        <p>{order.productList}</p>
+      </td>
+      <td>{order.rentalTime}</td>
+      <td>{order.date}</td>
+      <td>{order.status}</td>
+      <td>{order.price} Ron</td>
+    </tr>
+  ));
 
-    return(
-        <>
-        {errorMessage && <Alert variant="danger">A apărut o eroare în comunicarea cu serverul. Contactați departamentul IT!</Alert>}
+  useEffect(() => {
+    const recevingData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/v1/api/orders");
+        const responseData = await response.data;
+        setOrdersToShow(responseData);
+      } catch (error) {
+        setErrorMessage(true);
+        console.log(error.response);
+      }
+    };
+    recevingData();
+    getTotalComands();
+  }, [ordersToShow]);
+
+  return (
+    <>
+      {errorMessage && (
+        <Alert variant="danger">
+          A apărut o eroare în comunicarea cu serverul. Contactați departamentul
+          IT!
+        </Alert>
+      )}
+      <div className="order-table">
         <Table striped bordered hover responsive>
-            <thead>
-                <tr>
-                    <th>Nr. Comenzii</th>
-                    <th>Nume</th>
-                    <th>Prenume</th>
-                    <th>Telefon</th>
-                    <th>Produs</th>
-                    <th>Interval închiriere</th>
-                    <th>Status</th>
-                    <th>Pret</th>
-                    </tr>
-            </thead>
-            <tbody>
-               {ordersList}
-            </tbody>
+          <thead>
+            <tr>
+              <th>Nr. Comandă</th>
+              <th>Nume</th>
+              <th>Prenume</th>
+              <th>Telefon</th>
+              <th>Produs</th>
+              <th>Interval închiriere</th>
+              <th>Data</th>
+              <th>Status</th>
+              <th>Preț</th>
+            </tr>
+          </thead>
+          <tbody>{ordersList}</tbody>
         </Table>
-        <ViewOrderDetails order={orderView} show={orderModalShow} onHide={() => setOrderModalShow(false)}/>
-        </>
-    )
+      </div>
+      <div className="statistics-container">
+        <span className="statistics-text">Numărul total de comenzi: </span>
+        <span className="statistics-number">{numberOfOrders}</span>
+      </div>
+      <ViewOrderDetails
+        order={orderView}
+        show={orderModalShow}
+        onHide={() => setOrderModalShow(false)}
+      />
+    </>
+  );
 }
 
 export default OrdersTable;
