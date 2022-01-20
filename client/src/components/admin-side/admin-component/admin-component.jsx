1be33116dae6a7d5e00a0b2349dfb2b031a07555
catalinbanu <52399878+catalinbanu@users.noreply.github.com>
2022-01-20 19:27:33 +0200

    Date time picker feature (#6)
    * Implement date time picker for place order component
    
    * Add statistics

--- client/src/components/admin-side/admin-component/admin-component.jsx
+++ client/src/components/admin-side/admin-component/admin-component.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import './admin-component.css'
+import "./admin-component.css";
 import LeftPanelComponent from "../left-panel/left-panel";
 import OrdersTable from "../orders-table";
 
-function AdminComponent(){
-    return(
-        <div id="admin">
-            <LeftPanelComponent/>
-            <div id="right-panel">
-                <h1>Comenzi</h1>
-                <OrdersTable/>
-            </div>
-        </div>
-    )
+function AdminComponent() {
+  return (
+    <div id="admin">
+      <LeftPanelComponent />
+      <div id="right-panel">
+        <h1>Listă comenzi</h1>
+        <OrdersTable />
+      </div>
+    </div>
+  );
 }
 
 export default AdminComponent;
